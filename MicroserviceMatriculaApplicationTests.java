75b2fd77a8985bcc8923ef265c614182085f73c6
Daniel Ramos <dramosg@ucentral.edu.co>
2023-06-23 13:19:16 -0500

    add first strcture commit

---
+++ MicroserviceMatriculaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acelerati.microservice.microservice_matricula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceMatriculaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
